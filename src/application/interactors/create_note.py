7cc983a05c5402671b0f5676c41f2b618ad8ddcb
Stas <375292765815@mail.ru>
2024-05-13 01:25:54 +0300

    Enabled the creation of notes

--- src/application/interactors/create_note.py
+++ src/application/interactors/create_note.py
@@ -1,5 +1,7 @@
 from src.domain.models import EmptyNote, Note, TodoID
 
+from domain.exceptions import NoteIntegrityError, NoteAlreadyExist
+
 
 class CreateNote:
     def __init__(self, empty_note: EmptyNote, note_db_gateway, owner_id, todo_id: TodoID):
@@ -9,9 +11,12 @@ class CreateNote:
         self._todo_id = todo_id
 
     async def __call__(self) -> Note:
-        todo = await self._note_db_gateway.create_note(
-            note=self._note,
-            owner_id=self._owner_id,
-            todo_id=self._todo_id
-        )
-        return todo
+        try:
+            todo = await self._note_db_gateway.create_note(
+                note=self._note,
+                owner_id=self._owner_id,
+                todo_id=self._todo_id
+            )
+            return todo
+        except NoteIntegrityError as nie:
+            raise NoteAlreadyExist from nie
